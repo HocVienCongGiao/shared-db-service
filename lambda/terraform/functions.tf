4868d21636a00091fa5b97abe413ac596df60c25
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-06-11 11:28:38 +0700

    Update functions.tf

--- lambda/terraform/functions.tf
+++ lambda/terraform/functions.tf
@@ -1,7 +1,6 @@
 module "test1" {
   source = "./module"
 
-  query_api_source_arn = "local.query_api_gateway_execution_arn"
   service_name         = var.service_name
   function_name        = "test1"
   lambda_logs_name     = "test1"
