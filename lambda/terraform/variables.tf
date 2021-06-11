1a1a3c4693777ccace1152a8fdeac4b2ff6ac928
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-06-11 10:10:26 +0700

    Create variables.tf

---
+++ lambda/terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "service_name" {}
+
+variable "organisation" {
+  default = "HocVienCongGiao"
+}
+
+variable "environment" {
+  default = "dev-sg"
+}
+
+variable "app_type" {
+  default = "apps"
+}
+
+variable "aws_region" {
+  default = "ap-southeast-1"
+}
+
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "aws_account_id" {}
+
+variable "api_key" {
+  default = "DEFAULT_API_KEY_FOR_NOTIF"
+}
+
+variable "tfe_token" {}
