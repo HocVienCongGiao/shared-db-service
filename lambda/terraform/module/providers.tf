1a5870b907511373e7f8363242b9c83e2c1e2e25
ProjectHocVienCongGiao <84091963+ProjectHocVienCongGiao@users.noreply.github.com>
2021-06-14 20:48:47 +0700

    Update providers.tf

--- lambda/terraform/module/providers.tf
+++ lambda/terraform/module/providers.tf
@@ -1,5 +1,17 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.37.0"
+    }
     archive = "~> 1.3"
+
   }
 }
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
