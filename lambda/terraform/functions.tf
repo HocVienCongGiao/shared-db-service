4d7768e942e6f89afe3ab8d9d668640efaa2e56d
ProjectHocVienCongGiao <84091963+ProjectHocVienCongGiao@users.noreply.github.com>
2021-06-14 20:38:12 +0700

    Update functions.tf

--- lambda/terraform/functions.tf
+++ lambda/terraform/functions.tf
@@ -1,10 +1,11 @@
 module "test1" {
   source = "./module"
 
+  filename             = "migration"
   service_name         = var.service_name
   function_name        = "test1"
   lambda_logs_name     = "test1"
-  handler              = "main.test1"
+  handler              = "migration.test1"
   subnet_ids           = data.aws_subnet_ids.private.ids
   security_group_ids   = data.aws_security_groups.sg.ids
   aws_account_id       = var.aws_account_id
