d10c6a121a184d3766ffd21bb7a03d3be294f902
ProjectHocVienCongGiao <84091963+ProjectHocVienCongGiao@users.noreply.github.com>
2021-06-15 16:31:53 +0700

    Update migration.py

--- lambda/migration.py
+++ lambda/migration.py
@@ -20,7 +20,7 @@ from shared_db_app.migration import migrate
 # backend = get_backend("postgres://{}:{}@crossenv-hvcg.coivn3f0oomk.ap-southeast-1.rds.amazonaws.com/{}".format(DB_USER, DB_PASSWORD, DB_NAME))
 # migrations = read_migrations('./migrations')
 
-def migrate(event, context):
+def migration_handler(event, context):
     migrate()
 #     with backend.lock():
 #        # Apply any outstanding migrations
