ea016796b4be9bdf2c7855d37bfef4ea330ee68a
ProjectHocVienCongGiao <84091963+ProjectHocVienCongGiao@users.noreply.github.com>
2021-06-15 16:19:31 +0700

    Update migration.py

--- shared-db-app/migration.py
+++ shared-db-app/migration.py
@@ -16,7 +16,7 @@ DB_NAME =  os.getenv('DB_NAME')  # dev_hocvienconggiao
 
 # PostgreSQL: database connection
 backend = get_backend("postgres://{}:{}@crossenv-hvcg.coivn3f0oomk.ap-southeast-1.rds.amazonaws.com/{}".format(DB_USER, DB_PASSWORD, DB_NAME))
-migrations = read_migrations('./migrations')
+migrations = read_migrations('../migrations')
 
 def migrate():
     with backend.lock():
