c26109495c574e8d49eac0ac58145edcaba35133
nhuthm080280 <nhuthm080280@gmail.com>
2021-06-19 16:49:46 +0700

    expose db migration API

---
+++ lambda/terraform/api-gateway-integration/variables.tf
@@ -0,0 +1,36 @@
+//variable "is_query_api" {
+//  default = true
+//}
+//
+//variable "is_mutation_api" {
+//  default = true
+//}
+//
+//variable "is_auth_required" {
+//  default = true
+//}
+//
+//variable "rest_api_id" {}
+//
+//variable "resource_id" {}
+//
+//variable "path_part" {}
+//
+//variable "source_arn" {}
+//
+//variable "mutation_methods" {}
+//
+//variable "authorization" {}
+//
+//variable "integration_http_method" {}
+//
+//variable "type" {}
+//variable "organisation" {}
+
+variable "environment" {}
+
+variable "uri" {}
+
+variable "organisation" {
+  default = "HocVienCongGiao"
+}
