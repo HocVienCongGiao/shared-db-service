bc8a8934550e10f0e35759a8348f5c19b7e96113
nhuthm080280 <nhuthm080280@gmail.com>
2021-06-20 16:54:07 +0700

    update path part

--- lambda/terraform/api_gateway_integration.tf
+++ lambda/terraform/api_gateway_integration.tf
@@ -15,14 +15,14 @@ locals {
 resource "aws_api_gateway_resource" "this-proxy-query" {
   rest_api_id = aws_api_gateway_rest_api.db-migration-api.id
   parent_id   = aws_api_gateway_rest_api.db-migration-api.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "migrate"
 }
 
 // aws_api_gateway_method
 resource "aws_api_gateway_method" "this-proxy" {
   rest_api_id   = aws_api_gateway_rest_api.db-migration-api.id
   resource_id   = aws_api_gateway_resource.this-proxy-query.id
-  http_method   = "POST"
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
