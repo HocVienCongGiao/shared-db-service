380bc8a9b0c49f56521a649ac3479507e61fe89d
nhuthm080280 <nhuthm080280@gmail.com>
2021-06-20 13:53:32 +0700

    test output value

--- lambda/terraform/api_gateway_integration.tf
+++ lambda/terraform/api_gateway_integration.tf
@@ -27,14 +27,17 @@ resource "aws_api_gateway_method" "this-proxy" {
 }
 
 // aws_api_gateway_integration
+data "aws_lambda_function" "existing" {
+  function_name = "${var.environment}_${var.service_name}_migrate"
+}
 resource "aws_api_gateway_integration" "this-proxy-option" {
   rest_api_id = aws_api_gateway_rest_api.db-migration-api.id
   resource_id = aws_api_gateway_method.this-proxy.resource_id
   http_method = aws_api_gateway_method.this-proxy.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "https://gyji9vk7vh.execute-api.ap-southeast-1.amazonaws.com/db-migration-api" // TODO need to find correct uri
-//  uri                     = aws_lambda_function.this.invoke_arn
+//  uri                     = "https://gyji9vk7vh.execute-api.ap-southeast-1.amazonaws.com/db-migration-api" // TODO need to find correct uri
+  uri                     = data.aws_lambda_function.existing.invoke_arn
 }
 
 // aws_api_gateway_deployment
