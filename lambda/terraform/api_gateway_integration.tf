e6f96517c47b8aa098aab3a254c62c63b9a6d702
nhuthm080280 <nhuthm080280@gmail.com>
2021-06-20 16:50:32 +0700

    only apply for post method

--- lambda/terraform/api_gateway_integration.tf
+++ lambda/terraform/api_gateway_integration.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_resource" "this-proxy-query" {
 resource "aws_api_gateway_method" "this-proxy" {
   rest_api_id   = aws_api_gateway_rest_api.db-migration-api.id
   resource_id   = aws_api_gateway_resource.this-proxy-query.id
-  http_method   = "ANY"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
