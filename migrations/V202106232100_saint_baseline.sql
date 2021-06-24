8218e9431fc7de27c2edc3922abf79a56c31b53b
ntdat02092002 <datnt03092002@gmail.com>
2021-06-24 10:31:44 +0700

    update

--- migrations/V202106232100_saint_baseline.sql
+++ migrations/V202106232100_saint_baseline.sql
@@ -5,68 +5,78 @@ CREATE TABLE IF NOT EXISTS public.saint__saint
 CREATE TABLE IF NOT EXISTS public.saint__saint_english_name
 (
     id                 UUID PRIMARY KEY REFERENCES saint__saint(id),
-    english_name       VARCHAR
+    english_name       VARCHAR NOT NULL
 );
-CREATE INDEX IDX_saint_english_name ON saint__saint_english_name (english_name);
+CREATE UNIQUE INDEX IDX_saint_saint_english_name ON saint__saint_english_name (english_name);
 CREATE TABLE IF NOT EXISTS public.saint__saint_french_name
 (
     id                 UUID PRIMARY KEY REFERENCES saint__saint(id),
-    french_name        VARCHAR
+    french_name        VARCHAR NOT NULL
 );
-CREATE INDEX IDX_saint_french_name ON saint__saint_french_name (french_name);
+CREATE UNIQUE INDEX IDX_saint_saint_french_name ON saint__saint_french_name (french_name);
 CREATE TABLE IF NOT EXISTS public.saint__saint_latin_name
 (
     id                 UUID PRIMARY KEY REFERENCES saint__saint(id),
-    latin_name         VARCHAR
+    latin_name         VARCHAR NOT NULL
 );
-CREATE INDEX IDX_saint_latin_name ON saint__saint_latin_name (latin_name);
+CREATE UNIQUE INDEX IDX_saint_saint_latin_name ON saint__saint_latin_name (latin_name);
 CREATE TABLE IF NOT EXISTS public.saint__saint_vietnamese_name
 (
     id                 UUID PRIMARY KEY REFERENCES saint__saint(id),
     vietnamese_name    VARCHAR NOT NULL
 );
-CREATE INDEX IDX_saint_vietnamese_name ON saint__saint_vietnamese_name (vietnamese_name);
+CREATE UNIQUE INDEX IDX_saint_saint_vietnamese_name ON saint__saint_vietnamese_name (vietnamese_name);
+CREATE TABLE IF NOT EXISTS public.saint__saint_display_name
+(
+    id                 UUID PRIMARY KEY REFERENCES saint__saint(id),
+    display_name       VARCHAR NOT NULL
+);
+CREATE INDEX IDX_saint_saint_display_name ON saint__saint_display_name (display_name);
 CREATE TABLE IF NOT EXISTS public.saint__saint_gender
 (
     id                 UUID PRIMARY KEY REFERENCES saint__saint(id),
-    gender             VARCHAR NOT NULL
+    is_male            BOOLEAN NOT NULL
 );
-CREATE INDEX IDX_saint_gender ON saint__saint_gender (gender);
 CREATE TABLE IF NOT EXISTS public.saint__saint_feast_day
 (
     id                 UUID PRIMARY KEY REFERENCES saint__saint(id),
-    feast_day          VARCHAR NOT NULL
+    feast_day          SMALLINT NOT NULL,
+    feast_month        SMALLINT NOT NULL
 );
-CREATE INDEX IDX_saint_feast_day ON saint__saint_feast_day (feast_day);
+
 
 INSERT INTO public.saint__saint (id)
 VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608');
 
 INSERT INTO public.saint__saint_english_name (id, english_name)
-VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', NULL);
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'Peter the Apostle');
 
 INSERT INTO public.saint__saint_french_name (id, french_name)
-VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', NULL);
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'saint Pierre');
 
-INSERT INTO public.saint__saint_latin_name (id,latin_name)
-VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', NULL);
+INSERT INTO public.saint__saint_latin_name (id, latin_name)
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'Simon Petrus');
 
-INSERT INTO public.saint__saint_vietnamese_name (id,vietnamese_name)
-VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608','Lê Dang Thi');
+INSERT INTO public.saint__saint_vietnamese_name (id, vietnamese_name)
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608','Thánh Phêrô Tông đồ');
 
-INSERT INTO public.saint__saint_gender (id, gender)
-VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608','Female');
+INSERT INTO public.saint__saint_display_name (id, display_name)
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'Phêrô');
 
-INSERT INTO public.saint__saint_feast_day (id, feast_day)
-VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', '2410');
+INSERT INTO public.saint__saint_gender (id, is_male)
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', true);
+
+INSERT INTO public.saint__saint_feast_day (id, feast_day, feast_month)
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 29, 06);
 
 CREATE VIEW saint__saint_view AS
-    SELECT saint__saint.id, english_name, french_name, latin_name, vietnamese_name, gender, feast_day
+    SELECT saint__saint.id, english_name, french_name, latin_name, vietnamese_name, display_name, is_male, feast_day, feast_month
     FROM
     saint__saint
-    JOIN saint__saint_english_name ON saint__saint.id = saint__saint_english_name.id
-    JOIN saint__saint_french_name ON saint__saint.id = saint__saint_french_name.id
-    JOIN saint__saint_latin_name ON saint__saint.id = saint__saint_latin_name.id
-    JOIN saint__saint_vietnamese_name ON saint__saint.id = saint__saint_vietnamese_name.id
-    JOIN saint__saint_gender ON saint__saint.id = saint__saint_gender.id
-    JOIN saint__saint_feast_day ON saint__saint.id = saint__saint_feast_day.id
+    LEFT JOIN saint__saint_english_name ON saint__saint.id = saint__saint_english_name.id
+    LEFT JOIN saint__saint_french_name ON saint__saint.id = saint__saint_french_name.id
+    LEFT JOIN saint__saint_latin_name ON saint__saint.id = saint__saint_latin_name.id
+    LEFT JOIN saint__saint_vietnamese_name ON saint__saint.id = saint__saint_vietnamese_name.id
+    LEFT JOIN saint__saint_display_name ON saint__saint.id = saint__saint_display_name.id
+    LEFT JOIN saint__saint_gender ON saint__saint.id = saint__saint_gender.id
+    LEFT JOIN saint__saint_feast_day ON saint__saint.id = saint__saint_feast_day.id
