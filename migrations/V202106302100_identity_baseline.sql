7e70cdde54ed6d4ee244fb6755e87c5b150650e9
nhuthm080280 <nhuthm080280@gmail.com>
2021-07-01 15:57:06 +0700

    remove password table, add not null to enabled field

--- migrations/V202106302100_identity_baseline.sql
+++ migrations/V202106302100_identity_baseline.sql
@@ -21,23 +21,17 @@ CREATE TABLE IF NOT EXISTS public.identity__user_phone
     phone VARCHAR NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS public.identity__user_password
-(
-    id UUID PRIMARY KEY REFERENCES identity__user(id),
-    password VARCHAR NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS public.identity__user_enabled
 (
     id UUID PRIMARY KEY REFERENCES identity__user(id),
-    enabled bool default false
+    enabled bool NOT NULL
 );
 
 INSERT INTO public.identity__user (id)
 VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608');
 
 INSERT INTO public.identity__user_username (id, username)
-VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'hvcg_dev');
+VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'dev-operator');
 
 INSERT INTO public.identity__user_email (id, email)
 VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'projecthocvienconggiao@gmail.com');
@@ -45,14 +39,11 @@ VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'projecthocvienconggiao@gmail.co
 INSERT INTO public.identity__user_phone (id, phone)
 VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', '+84 909683979');
 
-INSERT INTO public.identity__user_password (id, password)
-VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', 'dummy_password');
-
 INSERT INTO public.identity__user_enabled (id, enabled)
 VALUES ('40e6215d-b5c6-4896-987c-f30f3678f608', true);
 
 CREATE VIEW identity__user_view AS
-SELECT identity__user.id, username, email, phone, password, enabled
+SELECT identity__user.id, username, email, phone, enabled
 FROM identity__user
          LEFT JOIN identity__user_username
                    ON identity__user.id = identity__user_username.id
@@ -60,7 +51,5 @@ FROM identity__user
                    ON identity__user.id = identity__user_email.id
          LEFT JOIN identity__user_phone
                    ON identity__user.id = identity__user_phone.id
-         LEFT JOIN identity__user_password
-                   ON identity__user.id = identity__user_password.id
          LEFT JOIN identity__user_enabled
                    ON identity__user.id = identity__user_enabled.id
