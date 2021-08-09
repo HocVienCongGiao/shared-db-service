0303abe8156bf26e977ac254ca4e6fcb79f89621
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-08-09 20:00:17 +0700

    Delete lambda-permission.tf

--- lambda/terraform/module/lambda-permission.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_lambda_permission" "this-migration-gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.this.function_name
-  principal     = "apigateway.amazonaws.com"
-  # The "/*/*" portion grants access from any method on any resource
-  # within the API Gateway REST API.
-//  source_arn = var.query_api_source_arn
-}
