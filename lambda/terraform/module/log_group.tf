9d55fa817e4914024c43b4b4585826ffd83ae965
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-08-09 20:24:33 +0700

    Update log_group.tf

--- lambda/terraform/module/log_group.tf
+++ lambda/terraform/module/log_group.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "this" {
-  name              = ${var.environment}_${var.service_name}_${var.lambda_logs_name} # Should be the same as function name
+  name              = "${var.environment}_${var.service_name}_${var.lambda_logs_name}" # var.lambda_logs_name Should be the same as function name
   retention_in_days = 14
 }
