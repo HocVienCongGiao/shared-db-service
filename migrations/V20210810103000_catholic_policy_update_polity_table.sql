449022448da5d0f201dff4d2918b7acc24417954
ntdat02092002 <datnt030902@gmail.com>
2021-08-10 11:43:39 +0700

    update type from char to varchar

---
+++ migrations/V20210810103000_catholic_policy_update_polity_table.sql
@@ -0,0 +1,37 @@
+DROP VIEW polity__parish_view;
+
+DROP VIEW polity__deanery_view;
+
+DROP VIEW polity__diocese_view;
+
+DROP VIEW polity__polity_view;
+
+ALTER TABLE public.polity__polity
+    ALTER COLUMN type TYPE VARCHAR;
+
+CREATE VIEW polity__polity_view AS
+    SELECT polity__polity.id, name, person_in_charge
+    FROM polity__polity
+    LEFT JOIN polity__polity_name ON polity__polity.id = polity__polity_name.id
+    LEFT JOIN polity__polity_person_in_charge ON polity__polity.id = polity__polity_person_in_charge.id;
+
+CREATE VIEW polity__province_view AS
+    SELECT polity.*, province.code
+    FROM polity__province province
+    LEFT JOIN polity__polity_view polity ON province.id = polity.id;
+
+CREATE VIEW polity__diocese_view AS
+    SELECT polity.*, diocese.province_id
+    FROM polity__diocese diocese
+    LEFT JOIN polity__polity_view polity ON diocese.id = polity.id;
+
+CREATE VIEW polity__deanery_view AS
+    SELECT polity.*, deanery.diocese_id
+    FROM polity__deanery deanery
+    LEFT JOIN polity__polity_view polity ON deanery.id = polity.id;
+
+CREATE VIEW polity__parish_view AS
+    SELECT polity.*, parish.deanery_id, deanery.diocese_id
+    FROM polity__parish parish
+    LEFT JOIN polity__polity_view polity ON parish.id = polity.id
+    LEFT JOIN polity__deanery_view deanery ON parish.deanery_id = deanery.id;
