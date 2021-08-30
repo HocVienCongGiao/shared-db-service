3fe1dbb0ef332db959395a10fd0456f84b47da7f
Peter Bean <peterbean410@gmail.com>
2021-08-30 15:12:23 +0700

    Fix enrolment__course_instance

--- migrations/V202108292000_enrolment_baseline.sql
+++ migrations/V202108292000_enrolment_baseline.sql
@@ -24,9 +24,9 @@ CREATE TABLE IF NOT EXISTS public.enrolment__specialism_instance_progress_school
 -- Course Instance
 CREATE TABLE IF NOT EXISTS public.enrolment__course_instance
 (
-    id                      UUID PRIMARY KEY,
-    course_id               UUID NOT NULL REFERENCES course__course(id),
-    specialism_instance_progress_id  UUID PRIMARY KEY REFERENCES enrolment__specialism_instance_progress(id),
+    id                              UUID PRIMARY KEY,
+    course_id                       UUID NOT NULL REFERENCES course__course(id),
+    specialism_instance_progress_id UUID NOT NULL REFERENCES enrolment__specialism_instance_progress(id),
     UNIQUE (course_id, specialism_instance_id)
 );
 
