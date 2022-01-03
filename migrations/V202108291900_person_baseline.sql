b0f16cab826c589d36b670cae1bb3f894a032c83
Minh-Hieu Pham <43398131+HieuMinh67@users.noreply.github.com>
2022-01-03 21:09:48 +0700

    [add] vow, foreign language (#18)

--- migrations/V202108291900_person_baseline.sql
+++ migrations/V202108291900_person_baseline.sql
@@ -42,6 +42,58 @@ CREATE TABLE IF NOT EXISTS public.person__person_last_name
     last_name          VARCHAR NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS public.person__person_nationality
+(
+    id                  UUID PRIMARY KEY REFERENCES person__person(id) ON DELETE CASCADE,
+    nationality         VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.person__person_race
+(
+    id                  UUID PRIMARY KEY REFERENCES person__person(id) ON DELETE CASCADE,
+    race                VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.person__person_id_number
+(
+    id                  UUID PRIMARY KEY,
+    person_id           UUID REFERENCES person__person(id) ON DELETE CASCADE,
+    person_id_number    VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS public.person__person_id_number_provider
+(
+    id                  UUID PRIMARY KEY REFERENCES person__person_id_number(id) ON DELETE CASCADE,
+    code                VARCHAR NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS public.person__person_id_number_date_of_issue
+(
+    id                       UUID PRIMARY KEY REFERENCES person__person_id_number(id) ON DELETE CASCADE,
+    date_of_issue            DATE NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS public.person__person_id_number_place_of_issue
+(
+    id                        UUID PRIMARY KEY REFERENCES person__person_id_number(id) ON DELETE CASCADE,
+    place_of_issue            VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.person__person_foreign_language
+(
+    id                          UUID PRIMARY KEY,
+    person_id                   UUID REFERENCES person__person(id) ON DELETE CASCADE,
+    language                    VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.person__person_foreign_language_level
+(
+    id                          UUID PRIMARY KEY REFERENCES person__person_foreign_language(id),
+    code                        VARCHAR NOT NULL
+);
+
 
 -- Student: Nguyễn Hữu Chiến
 INSERT INTO public.person__person (id, type)
@@ -64,6 +116,24 @@ VALUES ('53f549b9-99bf-4e12-88e3-c2f868953283', 'Hữu');
 
 INSERT INTO public.person__person_last_name (id, last_name)
 VALUES ('53f549b9-99bf-4e12-88e3-c2f868953283', 'Chiến');
+
+INSERT INTO public.person__person_nationality (id, nationality)
+VALUES ('53f549b9-99bf-4e12-88e3-c2f868953283', 'Vietnamese');
+
+INSERT INTO public.person__person_race (id, race)
+VALUES ('53f549b9-99bf-4e12-88e3-c2f868953283', 'Kinh');
+
+INSERT INTO public.person__person_id_number (id, person_id, person_id_number)
+VALUES ('2f6c0dd3-f6c7-4504-aba3-de017ad33821', '53f549b9-99bf-4e12-88e3-c2f868953283', '837837655558');
+
+INSERT INTO public.person__person_id_number_provider (id, code)
+VALUES ('2f6c0dd3-f6c7-4504-aba3-de017ad33821', 'NATIONAL_ID');
+
+INSERT INTO public.person__person_id_number_date_of_issue (id, date_of_issue)
+VALUES ('2f6c0dd3-f6c7-4504-aba3-de017ad33821', '2011-05-05');
+
+INSERT INTO public.person__person_id_number_place_of_issue (id, place_of_issue)
+VALUES ('2f6c0dd3-f6c7-4504-aba3-de017ad33821', 'TP.HCM');
 
 
 -- Teacher: Đỗ Văn Ngân
@@ -88,6 +158,36 @@ VALUES ('938c9343-2f5e-4517-8d2e-8f251403d350', 'Văn');
 INSERT INTO public.person__person_last_name (id, last_name)
 VALUES ('938c9343-2f5e-4517-8d2e-8f251403d350', 'Ngân');
 
+INSERT INTO public.person__person_nationality (id, nationality)
+VALUES ('938c9343-2f5e-4517-8d2e-8f251403d350', 'Vietnamese');
+
+INSERT INTO public.person__person_race (id, race)
+VALUES ('938c9343-2f5e-4517-8d2e-8f251403d350', 'Kinh');
+
+INSERT INTO public.person__person_id_number (id, person_id, person_id_number)
+VALUES ('f0dacf1c-b728-4072-8ded-495bcb9e9c57', '938c9343-2f5e-4517-8d2e-8f251403d350', '673487263555');
+
+INSERT INTO public.person__person_id_number_provider (id, code)
+VALUES ('f0dacf1c-b728-4072-8ded-495bcb9e9c57', 'NATIONAL_ID');
+
+INSERT INTO public.person__person_id_number_date_of_issue (id, date_of_issue)
+VALUES ('f0dacf1c-b728-4072-8ded-495bcb9e9c57', '2001-05-05');
+
+INSERT INTO public.person__person_id_number_place_of_issue (id, place_of_issue)
+VALUES ('f0dacf1c-b728-4072-8ded-495bcb9e9c57', 'TP.HCM');
+
+INSERT INTO public.person__person_foreign_language (id, person_id, language)
+VALUES ('ff45da19-8bae-4367-9d85-ac9b8dbe0b97', '938c9343-2f5e-4517-8d2e-8f251403d350', 'English');
+
+INSERT INTO public.person__person_foreign_language_level (id, code)
+VALUES ('ff45da19-8bae-4367-9d85-ac9b8dbe0b97', 'ADVANCED');
+
+INSERT INTO public.person__person_foreign_language (id, person_id, language)
+VALUES ('bdbca191-867f-4933-aa1a-eec10050e5bf', '938c9343-2f5e-4517-8d2e-8f251403d350', 'Latin');
+
+INSERT INTO public.person__person_foreign_language_level (id, code)
+VALUES ('bdbca191-867f-4933-aa1a-eec10050e5bf', 'ADVANCED');
+
 -- View
 CREATE VIEW person__person_christian_name_view AS
     SELECT person.id as person_id,
@@ -97,3 +197,45 @@ CREATE VIEW person__person_christian_name_view AS
     LEFT JOIN person__person_christian_names ON person.id = person__person_christian_names.person_id
     LEFT JOIN saint__saint_display_name ON person__person_christian_names.saint_id = saint__saint_display_name.id
     GROUP BY person.id;
+
+CREATE VIEW person__person_foreign_language_view AS
+SELECT language.id,
+       language.language,
+       person__person_foreign_language_level.code as level,
+       language.person_id                         as person_id
+FROM person__person_foreign_language as language
+         LEFT JOIN person__person_foreign_language_level on language.id = person__person_foreign_language_level.id
+
+DROP VIEW IF EXISTS person__person_view;
+CREATE VIEW person__person_view AS
+SELECT person.*,
+       person__person_title.title,
+       person__person_christian_name_view.saint_ids,
+    person__person_christian_name_view.christian_name,
+
+    person__person_first_name.first_name,
+    person__person_middle_name.middle_name,
+    person__person_last_name.last_name,
+    person__person_nationality.nationality,
+    person__person_race.race,
+    person__person_polity.polity_id,
+    polity.name polity_name,
+    polity.location_address polity_location_address,
+    polity.location_name polity_location_name,
+    polity.location_email polity_location_email
+    FROM person__person person
+    LEFT JOIN person__person_title ON person.id = person__person_title.id
+    LEFT JOIN person__person_first_name ON person.id = person__person_first_name.id
+    LEFT JOIN person__person_middle_name ON person.id = person__person_middle_name.id
+    LEFT JOIN person__person_last_name ON person.id = person__person_last_name.id
+    LEFT JOIN person__person_christian_name_view ON person.id = person__person_christian_name_view.person_id
+    LEFT JOIN person__person_nationality ON person.id = person__person_nationality.id
+    LEFT JOIN person__person_race ON person.id = person__person_race.id
+
+    LEFT JOIN person__person_id_number ppin on person.id = ppin.person_id
+    LEFT JOIN person__person_id_number_provider on ppin.id = person__person_id_number_provider.id
+    LEFT JOIN person__person_id_number_date_of_issue on ppin.id = person__person_id_number_date_of_issue.id
+    LEFT JOIN person__person_id_number_place_of_issue on ppin.id = person__person_id_number_place_of_issue.id
+
+    LEFT JOIN person__person_polity ON person.id = person__person_polity.id
+    LEFT JOIN polity__polity_view polity ON person__person_polity.polity_id = polity.id
--- migrations/V202108292000_student_baseline.sql
+++ migrations/V202108292000_student_baseline.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS public.student__person
+ CREATE TABLE IF NOT EXISTS public.student__person
 (
     id                 UUID PRIMARY KEY REFERENCES person__person(id) ON DELETE CASCADE
 );
@@ -24,13 +24,31 @@ CREATE TABLE IF NOT EXISTS public.student__student_email
 CREATE TABLE IF NOT EXISTS public.student__student_phone
 (
     id                 UUID PRIMARY KEY REFERENCES student__person(id) ON DELETE CASCADE,
-    phone              VARCHAR NOT NULL
+    phone              VARCHAR NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS public.student__student_address
+(
+    id                 UUID PRIMARY KEY REFERENCES student__person(id) ON DELETE CASCADE,
+    address              VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.student__student_educational_stage
+(
+    id                 UUID PRIMARY KEY REFERENCES student__person(id) ON DELETE CASCADE,
+    educational_stage              VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS public.student__student_undergraduate_school_name
 (
     id                 UUID PRIMARY KEY REFERENCES student__person(id) ON DELETE CASCADE,
     school_name        VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.student__student_vow_progress
+(
+    id                  UUID PRIMARY KEY REFERENCES student__person(id) ON DELETE CASCADE,
+    progress            VARCHAR NOT NULL
 );
 
 -- Student: Nguyễn Hữu Chiến
@@ -53,16 +71,16 @@ INSERT INTO public.student__student_undergraduate_school_name (id, school_name)
 VALUES ('53f549b9-99bf-4e12-88e3-c2f868953283', 'Đại Chủng Viện Thánh Quý - Cần Thơ');
 
 -- View
-
-CREATE VIEW student__student_view AS
+DROP VIEW IF EXISTS student__student_view;
+CREATE VIEW student__student_view1 AS
     SELECT student.*,
-    person__person_title.title,
+    person.title,
     person__person_christian_name_view.saint_ids,
     person__person_christian_name_view.christian_name,
 
-    person__person_first_name.first_name,
-    person__person_middle_name.middle_name,
-    person__person_last_name.last_name,
+    person.first_name,
+    person.middle_name,
+    person.last_name,
     student__student_date_of_birth.date_of_birth,
     student__student_place_of_birth.place_of_birth,
     student__student_email.email,
@@ -70,15 +88,12 @@ CREATE VIEW student__student_view AS
     student__student_undergraduate_school_name.school_name undergraduate_school_name,
 
     person__person_polity.polity_id,
-    polity.name polity_name,
-    polity.location_address polity_location_address,
-    polity.location_name polity_location_name,
-    polity.location_email polity_location_email
+    polity.name as polity_name,
+    polity.location_address as polity_location_address,
+    polity.location_name as polity_location_name,
+    polity.location_email as polity_location_email
     FROM student__person student
-    LEFT JOIN person__person_title ON student.id = person__person_title.id
-    LEFT JOIN person__person_first_name ON student.id = person__person_first_name.id
-    LEFT JOIN person__person_middle_name ON student.id = person__person_middle_name.id
-    LEFT JOIN person__person_last_name ON student.id = person__person_last_name.id
+    LEFT JOIN person__person_view person ON student.id = person.id
     LEFT JOIN student__student_date_of_birth ON student.id = student__student_date_of_birth.id
     LEFT JOIN student__student_place_of_birth ON student.id = student__student_place_of_birth.id
     LEFT JOIN student__student_email ON student.id = student__student_email.id
