67fb77a11ce8196b04aeec391b64b882e9f3fc09
Minh-Hieu Pham <43398131+HieuMinh67@users.noreply.github.com>
2022-01-11 22:57:09 +0700

    [ADD] missing educational stage fields (#22)
    * [ADD] missing educational stage fields
    
    * fix bug
    
    * [UPDATE] language, educational stage
    
    * [ADD] educational stage views
    
    * [REMOVE] redundant language tables
    
    * [update] person language
    
    * [fix] plural entity

--- migrations/V202108291900_person_baseline.sql
+++ migrations/V202108291900_person_baseline.sql
@@ -80,18 +80,12 @@ CREATE TABLE IF NOT EXISTS public.person__person_id_number_place_of_issue
     place_of_issue            VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.person__person_foreign_language
+CREATE TABLE IF NOT EXISTS public.person__person_languages
 (
-    id                          UUID PRIMARY KEY,
     person_id                   UUID NOT NULL REFERENCES person__person(id) ON DELETE CASCADE,
     language                    VARCHAR NOT NULL,
-    UNIQUE (person_id, language)
-);
-
-CREATE TABLE IF NOT EXISTS public.person__person_foreign_language_level
-(
-    id                          UUID PRIMARY KEY REFERENCES person__person_foreign_language(id),
-    code                        VARCHAR NOT NULL
+    level                       VARCHAR NOT NULL,
+    PRIMARY KEY (person_id, language)
 );
 
 CREATE TABLE IF NOT EXISTS public.person__person_date_of_birth
@@ -124,16 +118,41 @@ CREATE TABLE IF NOT EXISTS public.person__person_address
     address              VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.person__person_educational_stage
+CREATE TABLE IF NOT EXISTS public.person__educational_stage
 (
-    id                 UUID PRIMARY KEY REFERENCES person__person(id) ON DELETE CASCADE,
-    educational_stage              VARCHAR NOT NULL
+    id                UUID PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS public.person__educational_stage_educational_level
+(
+    id                UUID PRIMARY KEY REFERENCES person__educational_stage(id) ON DELETE CASCADE,
+    level             VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.person__educational_stage_school_name
+(
+    id          UUID PRIMARY KEY REFERENCES person__educational_stage (id) ON DELETE CASCADE,
+    school_name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.person__educational_stage_major
+(
+    id          UUID PRIMARY KEY REFERENCES person__educational_stage (id) ON DELETE CASCADE,
+    major       VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.person__person_educational_stages
+(
+    person_id               UUID REFERENCES person__person (id) ON DELETE CASCADE,
+    educational_stage_id    UUID REFERENCES person__educational_stage (id) ON DELETE CASCADE,
+    graduate_year           INT NOT NULL,
+    PRIMARY KEY (person_id, educational_stage_id, graduate_year)
 );
 
 CREATE TABLE IF NOT EXISTS public.person__person_vow_progress
 (
-    id                  UUID PRIMARY KEY REFERENCES person__person(id) ON DELETE CASCADE,
-    progress            VARCHAR NOT NULL
+    id       UUID PRIMARY KEY REFERENCES person__person (id) ON DELETE CASCADE,
+    progress VARCHAR NOT NULL
 );
 
 
@@ -192,6 +211,17 @@ VALUES ('2f6c0dd3-f6c7-4504-aba3-de017ad33821', '2011-05-05');
 INSERT INTO public.person__person_id_number_place_of_issue (id, place_of_issue)
 VALUES ('2f6c0dd3-f6c7-4504-aba3-de017ad33821', 'TP.HCM');
 
+INSERT INTO public.person__educational_stage (id)
+VALUES ('56439ecf-99e9-469a-9dcf-f5f654878e1b');
+
+INSERT INTO public.person__educational_stage_educational_level (id, level)
+VALUES ('56439ecf-99e9-469a-9dcf-f5f654878e1b', 'HIGH_SCHOOL');
+
+INSERT INTO public.person__educational_stage_school_name (id, school_name)
+VALUES ('56439ecf-99e9-469a-9dcf-f5f654878e1b', 'THPT Nguyễn Du');
+
+INSERT INTO public.person__person_educational_stages (person_id, educational_stage_id, graduate_year)
+VALUES ('53f549b9-99bf-4e12-88e3-c2f868953283', '56439ecf-99e9-469a-9dcf-f5f654878e1b', 2000);
 
 -- Teacher: Đỗ Văn Ngân
 INSERT INTO public.person__person (id)
@@ -233,17 +263,11 @@ VALUES ('f0dacf1c-b728-4072-8ded-495bcb9e9c57', '2001-05-05');
 INSERT INTO public.person__person_id_number_place_of_issue (id, place_of_issue)
 VALUES ('f0dacf1c-b728-4072-8ded-495bcb9e9c57', 'TP.HCM');
 
-INSERT INTO public.person__person_foreign_language (id, person_id, language)
-VALUES ('ff45da19-8bae-4367-9d85-ac9b8dbe0b97', '938c9343-2f5e-4517-8d2e-8f251403d350', 'English');
+INSERT INTO public.person__person_languages (person_id, language, level)
+VALUES ('938c9343-2f5e-4517-8d2e-8f251403d350', 'English', 'ADVANCED');
 
-INSERT INTO public.person__person_foreign_language_level (id, code)
-VALUES ('ff45da19-8bae-4367-9d85-ac9b8dbe0b97', 'ADVANCED');
-
-INSERT INTO public.person__person_foreign_language (id, person_id, language)
-VALUES ('bdbca191-867f-4933-aa1a-eec10050e5bf', '938c9343-2f5e-4517-8d2e-8f251403d350', 'Latin');
-
-INSERT INTO public.person__person_foreign_language_level (id, code)
-VALUES ('bdbca191-867f-4933-aa1a-eec10050e5bf', 'ADVANCED');
+INSERT INTO public.person__person_languages (person_id, language, level)
+VALUES ('938c9343-2f5e-4517-8d2e-8f251403d350', 'Latin', 'ADVANCED');
 
 -- View
 CREATE VIEW person__person_christian_name_view AS
@@ -255,53 +279,68 @@ CREATE VIEW person__person_christian_name_view AS
     LEFT JOIN saint__saint_display_name ON person__person_christian_names.saint_id = saint__saint_display_name.id
     GROUP BY person.id;
 
-CREATE VIEW person__person_foreign_language_view AS
-SELECT language.id,
-       language.language,
-       person__person_foreign_language_level.code as level,
-       language.person_id                         as person_id
-FROM person__person_foreign_language as language
-         LEFT JOIN person__person_foreign_language_level on language.id = person__person_foreign_language_level.id;
+CREATE VIEW person__educational_stage_view AS
+SELECT person__educational_stage.id,
+       educational_level.level as educational_level,
+       school_name.school_name,
+       major.major
+FROM person__educational_stage
+         LEFT JOIN person__educational_stage_school_name school_name ON person__educational_stage.id = school_name.id
+         LEFT JOIN person__educational_stage_educational_level educational_level
+                   ON person__educational_stage.id = educational_level.id
+         LEFT JOIN person__educational_stage_major major ON person__educational_stage.id = major.id;
+
+CREATE VIEW person__person_education_stage_view AS
+SELECT educational_stage_id, educational_level, school_name, major, graduate_year, person_id
+FROM person__person_educational_stages
+         LEFT JOIN person__educational_stage_view stage
+                   ON person__person_educational_stages.educational_stage_id = stage.id;
+
 
 CREATE VIEW person__person_view AS
 SELECT person.*,
        person__person_title.title,
        person__person_christian_name_view.saint_ids,
-    person__person_christian_name_view.christian_name,
+       person__person_christian_name_view.christian_name,
 
-    person__person_first_name.first_name,
-    person__person_middle_name.middle_name,
-    person__person_last_name.last_name,
-    person__person_date_of_birth.date_of_birth,
-    person__person_place_of_birth.place_of_birth,
-    person__person_email.email,
-    person__person_phone.phone,
-    person__person_nationality.nationality,
-    person__person_race.race,
-    person__person_polity.polity_id,
-    polity.name polity_name,
-    polity.location_address polity_location_address,
-    polity.location_name polity_location_name,
-    polity.location_email polity_location_email
-    FROM person__person person
+       person__person_first_name.first_name,
+       person__person_middle_name.middle_name,
+       person__person_last_name.last_name,
+       person__person_date_of_birth.date_of_birth,
+       person__person_place_of_birth.place_of_birth,
+       person__person_email.email,
+       person__person_phone.phone,
+       person__person_nationality.nationality,
+       person__person_race.race,
+       person__person_polity.polity_id,
+       polity.name             polity_name,
+       polity.location_address polity_location_address,
+       polity.location_name    polity_location_name,
+       polity.location_email   polity_location_email,
 
-    LEFT JOIN person__person_date_of_birth ON person.id = person__person_date_of_birth.id
-    LEFT JOIN person__person_place_of_birth ON person.id = person__person_place_of_birth.id
-    LEFT JOIN person__person_email ON person.id = person__person_email.id
-    LEFT JOIN person__person_phone ON person.id = person__person_phone.id
+       language.language,
+       language.level
+FROM person__person person
 
-    LEFT JOIN person__person_title ON person.id = person__person_title.id
-    LEFT JOIN person__person_first_name ON person.id = person__person_first_name.id
-    LEFT JOIN person__person_middle_name ON person.id = person__person_middle_name.id
-    LEFT JOIN person__person_last_name ON person.id = person__person_last_name.id
-    LEFT JOIN person__person_christian_name_view ON person.id = person__person_christian_name_view.person_id
-    LEFT JOIN person__person_nationality ON person.id = person__person_nationality.id
-    LEFT JOIN person__person_race ON person.id = person__person_race.id
+         LEFT JOIN person__person_date_of_birth ON person.id = person__person_date_of_birth.id
+         LEFT JOIN person__person_place_of_birth ON person.id = person__person_place_of_birth.id
+         LEFT JOIN person__person_email ON person.id = person__person_email.id
+         LEFT JOIN person__person_phone ON person.id = person__person_phone.id
 
-    LEFT JOIN person__person_id_number ppin on person.id = ppin.person_id
-    LEFT JOIN person__person_id_number_provider on ppin.id = person__person_id_number_provider.id
-    LEFT JOIN person__person_id_number_date_of_issue on ppin.id = person__person_id_number_date_of_issue.id
-    LEFT JOIN person__person_id_number_place_of_issue on ppin.id = person__person_id_number_place_of_issue.id
+         LEFT JOIN person__person_title ON person.id = person__person_title.id
+         LEFT JOIN person__person_first_name ON person.id = person__person_first_name.id
+         LEFT JOIN person__person_middle_name ON person.id = person__person_middle_name.id
+         LEFT JOIN person__person_last_name ON person.id = person__person_last_name.id
+         LEFT JOIN person__person_christian_name_view ON person.id = person__person_christian_name_view.person_id
+         LEFT JOIN person__person_nationality ON person.id = person__person_nationality.id
+         LEFT JOIN person__person_race ON person.id = person__person_race.id
 
-    LEFT JOIN person__person_polity ON person.id = person__person_polity.id
-    LEFT JOIN polity__polity_view polity ON person__person_polity.polity_id = polity.id
+         LEFT JOIN person__person_id_number ppin on person.id = ppin.person_id
+         LEFT JOIN person__person_id_number_provider on ppin.id = person__person_id_number_provider.id
+         LEFT JOIN person__person_id_number_date_of_issue on ppin.id = person__person_id_number_date_of_issue.id
+         LEFT JOIN person__person_id_number_place_of_issue on ppin.id = person__person_id_number_place_of_issue.id
+
+         LEFT JOIN person__person_polity ON person.id = person__person_polity.id
+         LEFT JOIN polity__polity_view polity ON person__person_polity.polity_id = polity.id
+
+         LEFT JOIN person__person_languages language ON person.id = language.person_id
